3e03087a13cd6ed17e40c8d302a4528f012a4cc2
Jon Bird <attid@Jons-Laptop>
2022-08-04 01:07:32 -0600

    start on index models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+User.belongsToMany(Post, {
+  through: ProductTag,
+  as: 'ProductTag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  through: ProductTag,
+  as: 'ProductTag'
+});
 
 module.exports = {
   Product,
