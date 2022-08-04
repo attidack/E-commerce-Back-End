3368b00adb7a94dae589d175fec9796a574d3f7f
Jon Bird <attid@Jons-Laptop>
2022-08-04 17:53:26 -0600

    finish tag update route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -72,18 +72,18 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  Category.update(req.body, {
+  Tag.update(req.body, {
     where: {
       id: req.params.id
     }
   }
 )
-  .then(dbCategoryData => {
-    if (!dbCategoryData) {
-      res.status(404).json({ message: 'No Category found with this id' });
+  .then(dbTagData  => {
+    if (!dbTagData ) {
+      res.status(404).json({ message: 'No tag found with this id' });
       return;
     }
-    res.json(dbCategoryData);
+    res.json(dbTagData );
   })
   .catch(err => {
     console.log(err);
